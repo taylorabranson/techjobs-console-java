683d5b6221e0141de53d1fa0be9e043c1fc5744e
Taylor Branson <taylor.a.branson@gmail.com>
2020-08-19 17:46:21 -0500

    implemented printJobs method

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -1,8 +1,7 @@
 package org.launchcode.techjobs.console;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.lang.reflect.Array;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -111,6 +110,17 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.size() == 0) {
+            System.out.println("No results");
+            return;
+        }
+        for (HashMap<String, String> job : someJobs) {
+            System.out.println("*****");
+            for (Map.Entry<String, String> field : job.entrySet()) {
+                System.out.println(field.getKey() + " : " + field.getValue());
+            }
+            System.out.println("*****\n");
+        }
+
     }
 }
